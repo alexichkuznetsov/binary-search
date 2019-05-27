4bc5e061fb85fdbe9f120d824051985ec6324c33
Aleksey <alexichkuznetsov@gmail.com>
2019-05-27 20:13:12 +0300

    Binary search algorithm done

---
+++ main.js
@@ -0,0 +1,19 @@
+function binarySearch(arr, searchTerm) {
+  let left = 0,
+    right = arr.length - 1,
+    middle = null;
+
+  while (left <= right) {
+    middle = Math.floor((left + right) / 2);
+
+    if (arr[middle] === searchTerm) {
+      return middle;
+    } else if (searchTerm < arr[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+
+  return -1;
+}
